5c86fb306a98b81bf1797226b6a78b377349832d
Mostafa Pirdadeh <116708385+MostafaPirdadeh@users.noreply.github.com>
2023-09-11 15:47:39 +0330

    add course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[courseList,setCourseList]=useState([]);
+  const[newCourse,setNewCourse]=useState("");
+  const handelChange=(event)=>{
+    setNewCourse(event.target.value)
+  }
+  const addCourse=()=>{
+    const newCourseList=[...courseList,newCourse]
+    setCourseList(newCourseList);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="add-course">
+        <input type='text' onChange={handelChange}></input>
+        <button onClick={addCourse}>Add Course</button>
+      </div>
+      {courseList.map((course)=>{
+        return <h1>{course}</h1>
+      })}
+      <div className='list'></div>
     </div>
   );
 }
